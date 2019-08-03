0880e6d0845673d4cd68b47376514ee619423a4d
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-03 21:11:01 +0200

    finished module 5

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if ((c.value != null && (isNaN(c.value) || c.value < min || c.value > max))) {
+      return { 'range': true };
+    }
+    return null;
+  };
+}
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null   {
+  const emailControl = c.get('email');
+  const confirmemailControl = c.get('confirmemail');
+  console.log('Checking that email matches');
+  if (emailControl.pristine || confirmemailControl.pristine) {
+    return null;
+  }
+  if (emailControl.value != null && confirmemailControl.value != null && emailControl.value !== confirmemailControl.value) {
+    return { mismatchEmail: true };
+  }
+  return null;
+}
+
+
+
 
 @Component({
   selector: 'app-customer',
@@ -27,9 +52,12 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(4)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       phone: '',
+      rating: [null, [ratingRange(1, 5)]],
       notification: ['email'],
-      //lastName: {value:'N/A', disabled:true},
-      email: ['', [Validators.required, Validators.email]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmemail: ['', [Validators.required]],
+      }, {validator: emailMatcher}),
       sendCatalog: true
     });
 
